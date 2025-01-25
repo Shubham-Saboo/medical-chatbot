508dffa20aad049d8d9da7eaaf5db645caf90fd7
shubhamsaboo <sabooshubham2000@gmail.com>
2025-01-25 17:34:54 -0500

    Connected LLM with memory

--- create_memory_for_llm.py
+++ create_memory_for_llm.py
@@ -3,6 +3,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 
+
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
