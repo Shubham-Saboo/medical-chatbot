6c94a78e776133cb645b7a20eaf068ccc08ac7f1
shubhamsaboo <sabooshubham2000@gmail.com>
2025-01-25 15:14:14 -0500

    Text splitted and chunks created

---
+++ create_memory_for_llm.py
@@ -0,0 +1,27 @@
+# Importing Packages
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+# Step 1: Load raw pdfs
+
+DATA_PATH = "data/"
+def load_pdf_files(data):
+    loader = DirectoryLoader(data,
+                             glob='*.pdf',
+                             loader_cls=PyPDFLoader)
+    documents = loader.load()
+    return documents
+
+documents = load_pdf_files(data = DATA_PATH)
+print('Length of PDF pages', len(documents))
+
+# Step 2: Create Chunks
+def create_chunks(extracted_data):
+    text_splitter=RecursiveCharacterTextSplitter(chunk_size=500,
+                                                 chunk_overlap=50)
+    text_chunks = text_splitter.split_documents(extracted_data)
+    return text_chunks
+
+text_chunks = create_chunks(extracted_data=documents)
+print("Length of text chunks:", len(text_chunks))
+ 
